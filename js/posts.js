38b5fa7ca09377309696ab28fcd8339a06423e4e
bsnun8 <bishnunath018@gmail.com>
2022-03-16 11:05:50 +0600

    Debug API, Network tab,Status code, headers, bad API

--- js/posts.js
+++ js/posts.js
@@ -33,3 +33,19 @@ function addAPost() {
     .then(res => res.json())
     .then(data => console.log(data))
 }
+
+function addAPost2() {
+  fetch('https://jsonplaceholder.typicode.com/pusts', {
+      method: 'POST',
+      body: JSON.stringify({
+          title: 'My new post',
+          body: 'This is my posts',
+          userId: 1
+      }),
+      headers: {
+          'Content-type': 'application/json; charset=UTF-8'
+      }
+  })
+      .then(res => res.json())
+      .then(data => console.log(data))
+}
